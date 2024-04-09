a0c12dbb37d7459e360032aa1545ef218ab59006
심현진 <130758785+hxeonxins@users.noreply.github.com>
2024-04-09 19:44:22 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    int t, n1, n2;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; ++i) {
+            scanf("%d %d", &n1, &n2);
+            printf("Case #%d: %d + %d = %d\n", i, n1, n2, n1+n2);
+    }
+    return 0;
+}
