c721bc079f9c70a2e263ef3c4c9fa58d6cb6a822
심현진 <130758785+hxeonxins@users.noreply.github.com>
2024-06-08 20:47:30 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    char ch1[21];
+    scanf("%s", ch1);
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n", ch1);
+    printf(":fan::fan::fan:\n");
+    
+    return 0;
+}
