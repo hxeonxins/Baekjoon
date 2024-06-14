80605558c0b1eb1102667e06e12c520a55dd2165
심현진 <130758785+hxeonxins@users.noreply.github.com>
2024-06-14 22:57:23 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int score[] = {12, 11, 11, 10, 9, 9, 9, 8 ,7, 6, 6};
+    int penalty[] = {1600, 894, 1327, 1311, 1004, 1178, 1357, 837, 1055, 556, 773};
+    
+    int n;
+    scanf("%d", &n);
+    printf("%d %d", score[n-1], penalty[n-1]);
+    
+    
+    return 0;
+}
