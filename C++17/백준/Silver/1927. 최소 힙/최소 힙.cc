b7485b0ed556d0dced9722faea029742486b452d
심현진 <130758785+hxeonxins@users.noreply.github.com>
2024-07-03 22:00:36 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <functional>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int n,temp;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> temp;
+        if(temp){
+            pq.push(temp);
+        } else {
+            if(pq.empty()){
+                cout << "0\n";
+            } else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+    }
+    
+    return 0;
+}
